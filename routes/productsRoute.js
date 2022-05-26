2a1026576ce3792863c684deeea78cc3df6371e4
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-26 23:22:34 +0600

    added get product by id route

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -2,12 +2,24 @@ const router = require('express').Router()
 const Products = require('../model/Products')
 
 
+// get all products
 router.get('/products', async (req, res) => {
     const products = await Products.find({})
 
     res.json({
         success: true,
         products
+    })
+})
+
+
+
+// get products by id
+router.get('/product/:id', async (req, res) => {
+    const product = await Products.findOne({_id: req.params.id})
+    res.json({
+        success: true,
+        product
     })
 })
 
