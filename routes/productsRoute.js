2105299d38f4e28b6993174e4510214ee32d37c0
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-27 22:44:02 +0600

    added add-product route

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -2,6 +2,26 @@ const router = require('express').Router()
 const Products = require('../model/Products')
 
 
+
+// add product
+router.post('/addProduct', async (req, res) => {
+    const { name, image, description, unit_price, Available_qty, minimum_order_qty } = req.body;
+    const newProduct = new Products({
+        name,
+        image,
+        description,
+        unit_price,
+        Available_qty,
+        minimum_order_qty
+    })
+
+    const product = await newProduct.save()
+    res.json({
+        success: true,
+        product
+    })
+})
+
 // get all products
 router.get('/products', async (req, res) => {
     const products = await Products.find({})
@@ -16,7 +36,7 @@ router.get('/products', async (req, res) => {
 
 // get products by id
 router.get('/product/:id', async (req, res) => {
-    const product = await Products.findOne({_id: req.params.id})
+    const product = await Products.findOne({ _id: req.params.id })
     res.json({
         success: true,
         product
