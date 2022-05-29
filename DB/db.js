7e49e4d6fab71b3b0b3157f7f744e63b8bdf2df9
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-30 02:26:55 +0600

    set mongodb atlas url

--- DB/db.js
+++ DB/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jnizw.mongodb.net/SADI-TECH-HOUSE?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.jnizw.mongodb.net/SADI-TECH-HOUSE?retryWrites=true&w=majority`;
 // const url = `mongodb://localhost:27017/sadi-tech-house`
 mongoose.connect(url, {
     useNewUrlParser: true,
