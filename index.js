f35b85eafdb9a1af27ae39582db4428963b332aa
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-30 02:34:15 +0600

    fixed environment var issues

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 6000
 require('dotenv').config()
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
