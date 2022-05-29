0ef522042c73a6594e9f061d0d2a0d80021200d7
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-30 02:09:21 +0600

    bug fixed

--- DB/db.js
+++ DB/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-const url = `mongodb://localhost:27017/sadi-tech-house`
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jnizw.mongodb.net/SADI-TECH-HOUSE?retryWrites=true&w=majority`;
+// const url = `mongodb://localhost:27017/sadi-tech-house`
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
